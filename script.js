03d694f71622cc08dc59f4be5073e7dd141285ce
h_ho <hohoang.dev@gmail.com>
2022-12-08 18:28:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+let container = document.getElementById('container');
+let count = 50;
+for(var i = 0; i<50; i++){
+    let leftSnow = Math.floor(Math.random() * container.clientWidth);
+    let topSnow = Math.floor(Math.random() * container.clientHeight);
+    let widthSnow = Math.floor(Math.random() * 50);
+    let timeSnow = Math.floor((Math.random() * 5) + 5);
+    let blurSnow = Math.floor(Math.random() * 10);
+    console.log(leftSnow);
+    let div = document.createElement('div');
+    div.classList.add('snow');
+    div.style.left = leftSnow + 'px';
+    div.style.top = topSnow + 'px';
+    div.style.width = widthSnow + 'px';
+    div.style.height = widthSnow + 'px';
+    div.style.animationDuration = timeSnow + 's';
+    div.style.filter = "blur(" + blurSnow + "px)";
+    container.appendChild(div);
+}
